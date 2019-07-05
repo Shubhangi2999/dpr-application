1be4f55f08e0b181c14bd50d87e1f53fb5ab1604
Shubhangi2999 <30768563+Shubhangi2999@users.noreply.github.com>
2019-07-05 08:37:35 +0530

    Add files via upload

--- display2.php
+++ display2.php
@@ -3,11 +3,12 @@
 
 <head>
 	<title></title>
+
 </head>
 <body>
 	<center>
 <?php
-if($_SERVER["REQUEST_METHOD"]=="POST"){
+if($_POST['Description']){
 	$desc_id="";
 	$obtained_value=array();
 	$target_value=array();
@@ -42,9 +43,10 @@ if(!$result) {
 
 
 if($result->num_rows > 0){
-	echo "<table id='table1' border=1>";
+	echo "<table id='table1' border=1 bgcolor='silver'>";
 	echo "<tr>";
 	echo "<th>"; echo "DATE OF INSERTION"; echo "</th>";
+	echo "<th>"; echo "DATE OF ENTRY"; echo "</th>";
 	echo "<th>"; echo "Obtained Value"; echo "</th>";
 	echo "<th>"; echo "Target Value"; echo "</th>";
 	echo "<th>"; echo "Values For Department"; echo "</th>";
@@ -54,21 +56,34 @@ if($result->num_rows > 0){
 
 
 while($row=$result->fetch_assoc()) {
+	$tmp=$row["Insertion_Date"];
 	echo "<tr>";
+	echo "<td>".$row["Insertion_Date"]."</td>";
 	echo "<td>".$row["Entry_date"]."</td>";
 	echo "<td>".$row["Obtained_value"]."</td>";
 	echo "<td>".$row["Target_Value"]."</td>";
 	echo "<td>".$row["Department_Name"]."</td>";
 	echo "<td>".$row["GradeName"]."</td>";
 	echo "<td>".$row["Remarks"]."</td>";
-	echo "<td>"; echo"<button type=submit>Edit</button>"; echo "</td>";
-	echo "<td>"; echo"<button type=submit>Delete</button>"; echo "</td>"; 
+	echo "<td>";
+    echo "<form method='POST' action='display2.php'>";
+    echo "<input type='hidden' name='row' value='$tmp'>";
+    echo " <input type='submit' value='Edit'>";
+    echo "</form>";
+	echo "</td>";
+	echo "<td>";
+    echo "<form method='POST' action='delete.php'>";
+    echo "<input type='hidden' name='row' value='$tmp'>";
+    echo " <input type='submit' value='Delete'>";
+    echo "</form>";
+	echo "</td>";
 	echo "</tr>";
 }
 	echo "</table>";
 }
 else {
-	echo "O RESULTS FETCHED";
+	echo "<p>";
+	echo "No results found!";
 }
 $conn->close();
 
